967ff8f57b588ef197a998645ef285481f79ccb6
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-12-08 07:47:13 -0500

    Added entire J.S. Bach set of 4-part Chorales as training material.  Added some items to TODO.

--- specs/vectors/midi/get_folk_songs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -O toc.html 'http://www.contemplator.com/war.html'
-
-for i in `grep href toc.html  | grep "<li>" | sed -e 's/^[^"]*"//' | sed -e 's/".*//' | sed -e 's/^/www.contemplator.com\//'`; do 
-wget -O /tmp/jbl $i
-wget `grep '\.mid' /tmp/jbl | sed -e 's/[^"]*"\.\.//' | sed -e 's/".*//' | sed -e 's/^/www.contemplator.com/'`
-done
-
