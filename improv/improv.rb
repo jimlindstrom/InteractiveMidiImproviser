dbd55a76c9f0fbb60ebf8664fb8158b3bdc8dd17
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-12-16 22:21:06 -0500

    Added data/test to repo.  Modified improv.rb to print out the cumulative surprises.

--- improv/improv.rb
+++ improv/improv.rb
@@ -3,13 +3,19 @@
 $LOAD_PATH << '.'
 require 'rubymusic_improv'
 
-num_vectors = 250 # there are 409 available
+num_training_vectors = 100 # there are 409 available
+num_testing_vectors  =  20
 # Training on 250 vectors eats up 840MB
 # That's over 3MB / vector
 
 puts "Training..."
 i = InteractiveImprovisor.new
-i.train num_vectors
+
+surprises = i.train(num_training_vectors, num_testing_vectors)
+surprises.each do |s|
+  puts "\t#{s[:critic].class}: #{s[:cum_surprise]}"
+end
+i.save("data/production")
 
 puts "Running..."
 i.run
