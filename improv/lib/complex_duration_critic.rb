2464f50932bb678866682e6900ea2e7fefac3748
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-12-31 13:12:02 -0500

    Moved duration generation into a complex critic.

---
+++ improv/lib/complex_duration_critic.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+class ComplexDurationCritic < Critic
+  def initialize(duration_critic, duration_and_beat_position_critic)
+    reset_cumulative_information_content
+
+    @duration_critic = duration_critic
+    @duration_and_beat_position_critic = duration_and_beat_position_critic
+  end
+
+  def reset
+    # do nothing...
+  end
+
+  def save(folder)
+    # do nothing...
+  end
+
+  def load(folder)
+    # do nothing...
+  end
+
+  def listen(note)
+    # FIXME this assumes all 3 sub-critics have already listened to the note
+    raise ArgumentError.new("not a note.  is a #{note.class}") if note.class != Music::Note
+    next_symbol = note.duration.to_symbol
+    expectations = get_expectations
+    if expectations.num_observations > 0
+      information_content = expectations.information_content(next_symbol.val)
+    else
+      information_content = Math::RandomVariable.max_information_content
+    end
+    add_to_cumulative_information_content information_content
+    return information_content
+  end
+
+  def get_expectations
+    # try generating from the history of {beat position; duration} symbols
+    e = @duration_and_beat_position_critic.get_expectations
+    return e if !e.nil? and e.num_observations > 0
+
+    # try generating just from the history of durations (independent of beat position)
+    e = @duration_critic.get_expectations
+    return e if !e.nil? and e.num_observations > 0
+
+    # still no luck?  then reset and just pick a random duration
+    @duration_critic.reset
+    e = @duration_critic.get_expectations
+    return e if !e.nil? and e.num_observations > 0
+
+    raise RuntimeError.new("Failed to generate a duration")
+  end
+end
--- improv/lib/duration_generator.rb
+++ improv/lib/duration_generator.rb
@@ -6,14 +6,15 @@ class DurationGenerator
   def initialize
     @critics = []
 
-    duration_critic_order = 3
-    @duration_critic = DurationCritic.new(duration_critic_order)
+    @duration_critic = DurationCritic.new(duration_critic_order=3)
     @critics.push @duration_critic
 
-    duration_critic_order = 2
-    duration_critic_lookahead = 3
-    @duration_and_beat_position_critic = DurationAndBeatPositionCritic.new(duration_critic_order, duration_critic_lookahead)
+    @duration_and_beat_position_critic = DurationAndBeatPositionCritic.new(duration_critic_order=2, duration_critic_lookahead=3)
     @critics.push @duration_and_beat_position_critic
+
+    # FIXME: order dependency.  Assumes that this will get called to listen() after the prev. two critics
+    @complex_duration_critic = ComplexDurationCritic.new(@duration_critic, @duration_and_beat_position_critic)
+    @critics.push @complex_duration_critic
   end
 
   def get_critics
@@ -25,26 +26,8 @@ class DurationGenerator
   end
 
   def generate
-    expectations = Math::RandomVariable.new(Music::Duration.num_values)
-
-    # try generating with knowledge of the beat position
-    duration_and_beat_position_exp = @duration_and_beat_position_critic.get_expectations
-    expectations += duration_and_beat_position_exp if !duration_and_beat_position_exp.nil?
-
+    expectations = @complex_duration_critic.get_expectations
     x = expectations.choose_outcome
-    return Music::Duration.new(x) if !x.nil?
-
-    # and try adding duration stats to it
-    duration_exp = @duration_critic.get_expectations
-    expectations += duration_exp if !duration_exp.nil?
-
-    x = expectations.choose_outcome
-    return Music::Duration.new(x) if !x.nil?
-
-    reset # we hit a space where we have no data.  reset and try a fallback plan
-
-    duration_exp = @duration_critic.get_expectations
-    x = duration_exp.choose_outcome
     return Music::Duration.new(x) if !x.nil?
 
     raise RuntimeError.new("Failed to generate a duration")
