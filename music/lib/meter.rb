7b745b018b46d469f0f498eb477937f0f0712c32
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-01-28 11:02:46 -0500

    Music::Meter was using 'subdiv' (nonstandard) instead of 'subbeat'. No longer.

--- music/lib/meter.rb
+++ music/lib/meter.rb
@@ -3,30 +3,30 @@
 module Music
   
   class Meter
-    attr_accessor :beats_per_measure, :beat_unit, :subdivs_per_beat
+    attr_accessor :beats_per_measure, :beat_unit, :subbeats_per_beat
   
-    def initialize(beats_per_measure, beat_unit, subdivs_per_beat)
+    def initialize(beats_per_measure, beat_unit, subbeats_per_beat)
       raise ArgumentError.new("beats_per_measure must be in 2..6") if beats_per_measure < 2 or beats_per_measure > 6
       raise ArgumentError.new("beat_unit must be in [2, 4, 8]") if [2, 4, 8].index(beat_unit).nil?
-      raise ArgumentError.new("subdivs_per_beat must be in [1, 2, 4]") if [1, 2, 4].index(subdivs_per_beat).nil?
+      raise ArgumentError.new("subbeats_per_beat must be in [1, 2, 4]") if [1, 2, 4].index(subbeats_per_beat).nil?
   
       @beats_per_measure = beats_per_measure
       @beat_unit         = beat_unit
-      @subdivs_per_beat  = subdivs_per_beat
+      @subbeats_per_beat = subbeats_per_beat
     end
   
     def self.random
       # this is the whole set of possibilities
       #beats_per_measure = Array(2..6).sample
       #beat_unit         = [2, 4, 8].sample
-      #subdivs_per_beat  = [1, 2, 4].sample
-      #return Meter.new(beats_per_measure, beat_unit, subdivs_per_beat)
+      #subbeats_per_beat = [1, 2, 4].sample
+      #return Meter.new(beats_per_measure, beat_unit, subbeats_per_beat)
   
       # this is a more common, constrained set of possibilities
       beats_per_measure = [3, 4].sample
       beat_unit         = [4].sample
-      subdivs_per_beat  = [1, 2].sample
-      return Meter.new(beats_per_measure, beat_unit, subdivs_per_beat)
+      subbeats_per_beat = [1, 2].sample
+      return Meter.new(beats_per_measure, beat_unit, subbeats_per_beat)
     end
   
     def self.num_values
@@ -39,12 +39,12 @@ module Music
       b.beat = 0
       b.beats_per_measure = @beats_per_measure
       b.subbeat = 0
-      b.subbeats_per_beat = @subdivs_per_beat
+      b.subbeats_per_beat = @subbeats_per_beat
       return b
     end
   
     def val
-      { :beats_per_measure => @beats_per_measure, :beat_unit => @beat_unit, :subdivs_per_beat => @subdivs_per_beat }
+      { :beats_per_measure => @beats_per_measure, :beat_unit => @beat_unit, :subbeats_per_beat => @subbeats_per_beat }
     end
   
   end
--- music/specs/meter_spec.rb
+++ music/specs/meter_spec.rb
@@ -10,53 +10,53 @@ describe Music::Meter do
     before(:each) do
       @beats_per_measure = 3 # 3/4 time
       @beat_unit         = 4
-      @subdivs_per_beat  = 2 # expressed in eight notes
+      @subbeats_per_beat = 2 # expressed in eight notes
     end
-    it "should take 3 values: beats/measure, a beat unit, and subdivisions/beat" do
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+    it "should take 3 values: beats/measure, a beat unit, and subbeats/beat" do
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
 
     it "should take beats/measure of 2 through 6" do
       @beats_per_measure = 2 # 3/4 time
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
     it "should take beats/measure of 2 through 6" do
       @beats_per_measure = 6 # 6/4 time
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
 
     it "should take beat units of 2, 4, or 8" do
       @beat_unit = 2 # 3/2 time
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
     it "should take beat units of 2, 4, or 8" do
       @beat_unit = 4 # 3/4 time
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
     it "should take beat units of 2, 4, or 8" do
       @beat_unit = 8 # 3/8 time
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
 
-    it "should take subdivs/beat of 1, 2, or 4" do
-      @subdivs_per_beat = 1 # expressed in the beat unit
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+    it "should take subbeats/beat of 1, 2, or 4" do
+      @subbeats_per_beat = 1 # expressed in the beat unit
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
-    it "should take subdivs/beat of 1, 2, or 4" do
-      @subdivs_per_beat = 4 # expressed 4ths of the beat unit
-      Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat).should be_an_instance_of Music::Meter
+    it "should take subbeats/beat of 1, 2, or 4" do
+      @subbeats_per_beat = 4 # expressed 4ths of the beat unit
+      Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat).should be_an_instance_of Music::Meter
     end
 
     it "raise an error on invalid parameters" do
       lambdas = []
-      lambdas.push lambda { Music::Meter.new(1,                  @beat_unit, @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(7,                  @beat_unit, @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 1,          @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 3,          @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 5,          @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 6,          @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 7,          @subdivs_per_beat) }
-      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 9,          @subdivs_per_beat) }
+      lambdas.push lambda { Music::Meter.new(1,                  @beat_unit, @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(7,                  @beat_unit, @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 1,          @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 3,          @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 5,          @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 6,          @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 7,          @subbeats_per_beat) }
+      lambdas.push lambda { Music::Meter.new(@beats_per_measure, 9,          @subbeats_per_beat) }
       lambdas.push lambda { Music::Meter.new(@beats_per_measure, @beat_unit, 0                ) }
       lambdas.push lambda { Music::Meter.new(@beats_per_measure, @beat_unit, 3                ) }
       lambdas.push lambda { Music::Meter.new(@beats_per_measure, @beat_unit, 5                ) }
@@ -89,8 +89,8 @@ describe Music::Meter do
     it "should get the number of beats per measure" do
       @beats_per_measure = 2 # 3/4 time
       @beat_unit         = 2
-      @subdivs_per_beat  = 1 # expressed in eight notes
-      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat)
+      @subbeats_per_beat = 1 # expressed in eight notes
+      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat)
       m.beats_per_measure.should equal 2
     end
   end
@@ -99,19 +99,19 @@ describe Music::Meter do
     it "should get beat unit" do
       @beats_per_measure = 2 # 3/4 time
       @beat_unit         = 2
-      @subdivs_per_beat  = 1 # expressed in eight notes
-      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat)
+      @subbeats_per_beat = 1 # expressed in eight notes
+      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat)
       m.beat_unit.should equal 2
     end
   end
 
-  context "subdivs_per_beat" do
-    it "should get number of subdivisions per beat" do
+  context "subbeats_per_beat" do
+    it "should get number of subbeats per beat" do
       @beats_per_measure = 2 # 3/4 time
       @beat_unit         = 2
-      @subdivs_per_beat  = 1 # expressed in eight notes
-      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat)
-      m.subdivs_per_beat.should equal 1
+      @subbeats_per_beat = 1 # expressed in eight notes
+      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat)
+      m.subbeats_per_beat.should equal 1
     end
   end
 
@@ -119,9 +119,9 @@ describe Music::Meter do
     it "should return all values" do
       @beats_per_measure = 2 # 3/4 time
       @beat_unit         = 2
-      @subdivs_per_beat  = 1 # expressed in eight notes
-      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subdivs_per_beat)
-      m.val.should == {:beats_per_measure => 2, :beat_unit => 2, :subdivs_per_beat => 1}
+      @subbeats_per_beat = 1 # expressed in eight notes
+      m = Music::Meter.new(@beats_per_measure, @beat_unit, @subbeats_per_beat)
+      m.val.should == {:beats_per_measure => 2, :beat_unit => 2, :subbeats_per_beat => 1}
     end
   end
 
