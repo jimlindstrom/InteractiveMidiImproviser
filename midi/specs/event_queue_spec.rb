e3db9c9519f61ae6c79a1bbab3dd0e1a9c6fbf3c
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-01-29 11:19:29 -0500

    Wrote tests for existing-but-untested features.

--- midi/specs/event_queue_spec.rb
+++ midi/specs/event_queue_spec.rb
@@ -50,4 +50,43 @@ describe Midi::EventQueue do
     end
   end
 
+  describe ".get_pitches" do
+    it "should return an array of all the pitches" do
+      @evq = Midi::EventQueue.new
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 40, :velocity => 100, :timestamp => 1000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 40, :velocity => 100, :timestamp => 2000 }))
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 50, :velocity => 100, :timestamp => 2000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 50, :velocity => 100, :timestamp => 3000 }))
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 60, :velocity => 100, :timestamp => 3000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 60, :velocity => 100, :timestamp => 4000 }))
+      @evq.get_pitches.should == [40, 50, 60]
+    end
+  end
+
+  describe ".get_interonset_intervals" do
+    it "should return an array of the onset-to-onset timestamp deltas" do
+      @evq = Midi::EventQueue.new
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 40, :velocity => 100, :timestamp => 1000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 40, :velocity => 100, :timestamp => 2000 }))
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 50, :velocity => 100, :timestamp => 2000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 50, :velocity => 100, :timestamp => 3000 }))
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 60, :velocity => 100, :timestamp => 3000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 60, :velocity => 100, :timestamp => 4000 }))
+      @evq.get_interonset_intervals.should == [2000-1000,  3000-2000]
+    end
+  end
+
+  describe ".get_last_duration" do
+    it "should return the duration (note-on to note-off time) of the last note" do
+      @evq = Midi::EventQueue.new
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 40, :velocity => 100, :timestamp => 1000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 40, :velocity => 100, :timestamp => 2000 }))
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 50, :velocity => 100, :timestamp => 2000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 50, :velocity => 100, :timestamp => 3000 }))
+      @evq.enqueue(Midi::NoteOnEvent.new( { :pitch => 60, :velocity => 100, :timestamp => 3000 }))
+      @evq.enqueue(Midi::NoteOffEvent.new({ :pitch => 60, :velocity => 100, :timestamp => 4000 }))
+      @evq.get_last_duration.should == 1000
+    end
+  end
+
 end
