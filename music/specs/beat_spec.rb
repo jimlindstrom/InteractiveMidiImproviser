2cc6c8be6e0d71eed8ba277b8d630c751c34b089
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-01-29 18:01:09 -0500

    Refactoring of specs to improve readability

--- music/specs/beat_spec.rb
+++ music/specs/beat_spec.rb
@@ -3,44 +3,29 @@
 require 'spec_helper'
 
 describe Music::Beat do
-
-  before(:each) do
-  end
-
-  context "similarity_to" do
-    it "should return 0.0 when compared to nil" do
+  describe ".similarity_to" do
+    before(:all) do
       n0 = Music::Note.new(Music::Pitch.new(50), Music::Duration.new(1))
       n1 = Music::Note.new(Music::Pitch.new(52), Music::Duration.new(2))
-      b = Music::Beat.new
-      b.prev_note = n0
-      b.cur_note  = n1
-
-      b.similarity_to(nil).should be_within(0.01).of(0.0)
+      @b = Music::Beat.new
+      @b.prev_note = n0
+      @b.cur_note  = n1
     end
-    it "should return 0.0 when compared to a radically different duration" do
-      n0 = Music::Note.new(Music::Pitch.new(50), Music::Duration.new(2))
-      n1 = Music::Note.new(Music::Pitch.new(52), Music::Duration.new(1))
-      b = Music::Beat.new
-      b.prev_note = n0
-      b.cur_note  = n1
 
+    it "should return 1.0 when compared to itself" do
+      @b.similarity_to(@b).should be_within(0.01).of(1.0)
+    end
+    it "should return 0.0 when compared to nil" do
+      @b.similarity_to(nil).should be_within(0.01).of(0.0)
+    end
+    it "should return 0.0 when compared to a radically different beat" do
       n0 = Music::Note.new(Music::Pitch.new(100), Music::Duration.new(2))
       n1 = Music::Note.new(Music::Pitch.new(10), Music::Duration.new(20))
-      b2 = Music::Beat.new
-      b2.prev_note = n0
-      b2.cur_note  = n1
+      @b2 = Music::Beat.new
+      @b2.prev_note = n0
+      @b2.cur_note  = n1
 
-      b.similarity_to(b2).should be_within(0.01).of(0.0)
-    end
-    it "should return 1.0 when compared to itself" do
-      n0 = Music::Note.new(Music::Pitch.new(50), Music::Duration.new(1))
-      n1 = Music::Note.new(Music::Pitch.new(50), Music::Duration.new(1))
-      b = Music::Beat.new
-      b.prev_note = n0
-      b.cur_note  = n1
-
-      b.similarity_to(b).should be_within(0.01).of(1.0)
+      @b.similarity_to(@b2).should be_within(0.01).of(0.0)
     end
   end
-
 end
--- music/specs/phrase_spec.rb
+++ music/specs/phrase_spec.rb
@@ -3,36 +3,28 @@
 require 'spec_helper'
 
 describe Music::Phrase do
-  before do
+  before(:each) do
+    @vector = $phrasing_vectors["Bring back my bonnie to me"]
+    @nq = @vector[:note_queue]
   end
 
-  context "new" do
+  describe ".new" do
     it "should take a note queue, a start (note) index and an end (note) index" do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      nq = vector[:note_queue]
-      Music::Phrase.new(nq, idx1=2, idx2=8).should be_an_instance_of Music::Phrase
+      Music::Phrase.new(@nq, idx1=2, idx2=8).should be_an_instance_of Music::Phrase
     end
     it "should raise an error if the indices are out of bounds" do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      nq = vector[:note_queue]
-      expect { Music::Phrase.new(nq, idx1=-1, idx2=8) }.to raise_error
+      expect { Music::Phrase.new(@nq, idx1=-1, idx2=8) }.to raise_error
     end
     it "should raise an error if the indices are out of bounds" do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      nq = vector[:note_queue]
-      expect { Music::Phrase.new(nq, idx1=1, idx2=nq.length) }.to raise_error
+      expect { Music::Phrase.new(@nq, idx1=1, idx2=nq.length) }.to raise_error
     end
     it "should raise an error if the indices are out of order" do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      nq = vector[:note_queue]
-      expect { Music::Phrase.new(nq, idx1=5, idx2=3) }.to raise_error
+      expect { Music::Phrase.new(@nq, idx1=5, idx2=3) }.to raise_error
     end
   end
 
-  context "notes" do
-    before(:all) do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      @nq = vector[:note_queue]
+  describe ".notes" do
+    before(:each) do
       @p = Music::Phrase.new(@nq, @idx1=2, @idx2=8)
     end
     it "should return a note_queue" do
@@ -44,10 +36,8 @@ describe Music::Phrase do
     end
   end
 
-  context "duration" do
-    before(:all) do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      @nq = vector[:note_queue]
+  describe ".duration" do
+    before(:each) do
       @p = Music::Phrase.new(@nq, @idx1=2, @idx2=8)
     end
     it "should return the sum of the duration of the individual notes" do
@@ -56,10 +46,8 @@ describe Music::Phrase do
     end
   end
 
-  context "length" do
-    before(:all) do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      @nq = vector[:note_queue]
+  describe ".length" do
+    before(:each) do
       @p = Music::Phrase.new(@nq, @idx1=2, @idx2=8)
     end
     it "should return the number of individual notes" do
@@ -68,10 +56,9 @@ describe Music::Phrase do
     end
   end
 
-  context "score" do
-    before(:all) do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      @nq = vector[:note_queue]
+  describe ".score" do
+    before(:each) do
+      @nq.create_intervals
       @p = Music::Phrase.new(@nq, @idx1=2, @idx2=8)
     end
     it "should return a floating point value" do
@@ -79,36 +66,39 @@ describe Music::Phrase do
     end
   end
 
-  context "split_at_a_big_interval" do
+  describe ".split_at_a_big_interval" do
     before(:each) do
-      vector = $phrasing_vectors["Bring back my bonnie to me"]
-      @nq = vector[:note_queue]
       @nq.create_intervals
     end
-    it "should raise an error for phrases with fewer than two notes" do
-      p = Music::Phrase.new(@nq, idx1=2, idx2=2)
-      expect { p.split_at_a_big_interval }.to raise_error
+    context "for phrases with 0 or 1 notes" do
+      it "should raise an error" do
+        @one_note_phrase = Music::Phrase.new(@nq, idx1=2, idx2=2)
+        expect { @one_note_phrase.split_at_a_big_interval }.to raise_error
+      end
     end
-    it "should return a new phrase, for phrases containing two or more notes" do
-      p = Music::Phrase.new(@nq, idx1=2, idx2=3)
-      p.split_at_a_big_interval.should be_an_instance_of Music::Phrase
-    end
-    it "should decrement its own ending index by at least 1" do
-      p = Music::Phrase.new(@nq, idx1=2, idx2=3)
-      p.split_at_a_big_interval
-      p.end_idx.should be < 3
-    end
-    it "should return a new phrase starting right after the newly-split current phrase" do
-      p = Music::Phrase.new(@nq, idx1=2, idx2=7)
-      p2 = p.split_at_a_big_interval
-      p.end_idx.should == (p2.start_idx-1)
-    end
-    it "should return a new phrase ending at the end of the original phrase" do
-      p = Music::Phrase.new(@nq, idx1=2, idx2=7)
-      p2 = p.split_at_a_big_interval
-      p2.end_idx.should == idx2
+    context "for phrases with 2 or more notes" do
+      before(:each) do
+        @two_note_phrase = Music::Phrase.new(@nq, idx1=2, idx2=3)
+        @six_note_phrase = Music::Phrase.new(@nq, idx1=2, idx2=7)
+      end
+      it "should return a new phrase" do
+        @two_note_phrase.split_at_a_big_interval.should be_an_instance_of Music::Phrase
+      end
+      it "should decrement its own ending index by at least 1" do
+        old_end_idx = @two_note_phrase.end_idx
+        @two_note_phrase.split_at_a_big_interval
+        @two_note_phrase.end_idx.should be < old_end_idx
+      end
+      it "should return a new phrase starting right after the newly-split current phrase" do
+        p2 = @six_note_phrase.split_at_a_big_interval
+        @six_note_phrase.end_idx.should == (p2.start_idx-1)
+      end
+      it "should return a new phrase ending at the end of the original phrase" do
+        old_end_idx = @six_note_phrase.end_idx
+        p2 = @six_note_phrase.split_at_a_big_interval
+        p2.end_idx.should == old_end_idx
+      end
     end
   end
-
 
 end
